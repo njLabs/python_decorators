6ea9613cc8cf795d1cd0db5af5007780db7adb72
Navjot <50581381+njLabs@users.noreply.github.com>
2024-04-24 11:46:29 +0530

    Update useful_decorators.py
    Profiles any function in following class just by adding @profile above function

--- useful_decorators.py
+++ useful_decorators.py
@@ -1,4 +1,7 @@
 import time
+import cProfile
+import pstats
+import io
 
 
 def time_it(func):
@@ -31,3 +34,24 @@ def log_decorator(func):
         return func(*args, **kwargs)
 
     return wrapper
+
+
+def profile(fnc):
+    """
+    Profiles any function in following class just by adding @profile above function
+    """
+    def inner(*args, **kwargs):
+        pr = cProfile.Profile()
+        pr.enable()
+        retval = fnc(*args, **kwargs)
+        pr.disable()
+        s = io.StringIO()
+        sortby = 'cumulative'  # Ordered
+        ps = pstats.Stats(pr, stream=s).strip_dirs().sort_stats(sortby)
+        n = 10  # reduced the list to be monitored
+        ps.print_stats(n)
+        # ps.dump_stats("profile.prof")
+        print(s.getvalue())
+        return retval
+
+    return inner
